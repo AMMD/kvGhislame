e6b63c105fa2ea5e692502eab4da19716790fe11
orl <orl@debian>
2012-11-25 09:21:38 +0100

    ValueFader colorized

--- valuefader.py
+++ valuefader.py
@@ -4,11 +4,16 @@ kivy.require('1.4.1')
 from kivy.app import App
 from kivy.uix.slider import Slider
 from kivy.uix.widget import Widget
-from kivy.properties import StringProperty, ObjectProperty, OptionProperty
+from kivy.properties import StringProperty, ObjectProperty, OptionProperty, NumericProperty, ReferenceListProperty
 from kivy.factory import Factory
 
 class Fader(Slider):
     path = StringProperty('Fader')
+    # Couleur
+    r = NumericProperty()
+    g = NumericProperty()
+    b = NumericProperty()
+    color = ReferenceListProperty(r, g, b)
 #    def on_touch_move(self, touch):
 #        app = App.get_running_app()
 #        app.send_message()
@@ -17,11 +22,22 @@ class ValueFader(Widget):
     fader = ObjectProperty(None)
     name_f = ObjectProperty(None)
     name = StringProperty()
+
+    # Couleur
+    r = NumericProperty()
+    g = NumericProperty()
+    b = NumericProperty()
+    color = ReferenceListProperty(r, g, b)
+    
+    # Fader settings
     orientation = OptionProperty('horizontal', options=('vertical', 'horizontal'))
+    max = NumericProperty()
+    min = NumericProperty()
+    # (require 1.4.2) step = NumericProperty()    
 
 class ValueFaderApp(App):
     def build(self):
-        return ValueFader(name='My Fader')
+        return ValueFader(name='My Fader', orientation='horizontal', color=(1,0.5,0.5))
 
 Factory.register('Fader', Fader)
 if __name__ == "__main__":
