62b5b39fe4bf226ffb5232715de3bb2b79a54510
orl <orl@debian>
2012-11-25 11:00:56 +0100

    XyPad en cours

--- xypad.py
+++ xypad.py
@@ -7,18 +7,41 @@ from kivy.properties import StringProperty, NumericProperty, ReferenceListProper
 from kivy.factory import Factory
 
 class InnerBox(Widget):
+    # Inner Color
     h = NumericProperty()
     s = NumericProperty()
     v = NumericProperty()
-    color = ReferenceListProperty(h, s, v)
+    i_color = ReferenceListProperty(h, s, v)
+
+    # Outer Color
+    r = NumericProperty()
+    g = NumericProperty()
+    b = NumericProperty()
+    color = ReferenceListProperty(r, g, b)
 
 class XyPad(Widget):
     innerbox = ObjectProperty(None)
     path = StringProperty('xypad')
+    name = StringProperty()
+    name_w = ObjectProperty(None)
+
+    # Outer Color
+    r = NumericProperty()
+    g = NumericProperty()
+    b = NumericProperty()
+    color = ReferenceListProperty(r, g, b)
+
+    # Axes
+    x_name = StringProperty()
+    x_name_w = ObjectProperty(None)
+    y_name = StringProperty()
+    y_name_w = ObjectProperty(None)
+
+
 
 class XyPadApp(App):
     def build(self):
-        return XyPad();
+        return XyPad(color=(1, 1, 1), name='My XY Pad', x_name='X', y_name='Y');
 
 Factory.register('InnerBox', InnerBox)
 if __name__ == '__main__':
