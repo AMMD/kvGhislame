691fed3e74c7896d6a111f810f2d7b77295f9eb4
orlammd <orl@ammd.net>
2012-12-09 21:57:37 +0100

    pb de proportionnalité des Extended XyPad résolu ?

--- extendedxypad.py
+++ extendedxypad.py
@@ -15,13 +15,14 @@ class ExtendedXyPad(XyPad):
     yfader = ObjectProperty(Fader)
                     
     def update_cursor_from_pad(self, obj, touch):
-        if obj.collide_point(touch.pos[0], touch.pos[1]):
+        if touch.x < obj.x + obj.width + 5:
             self.xfader.value_pos = touch.pos
+        if touch.y < obj.y + obj.height + 5:
             self.yfader.value_pos = touch.pos
 
 class ExtendedXyPadApp(App):
     def build(self):
-        return ExtendedXyPad(color=(1,0,0), name='My Xtended XY Pad', x_name='X', y_name='Y')
+        return ExtendedXyPad(color=(1,0,0), name='My Xtended XY Pad', x_name='X', y_name='Y', step=(0.01, 0.01))
 
 
 #Factory.register('XyPad', XyPad)
