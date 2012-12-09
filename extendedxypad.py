5549a07135d2553f605abb943a3e2c08efe388df
orlammd <orl@ammd.net>
2012-12-09 11:22:53 +0100

    tourne en rond - pas de sortie

--- extendedxypad.py
+++ extendedxypad.py
@@ -6,16 +6,19 @@ from kivy.uix.widget import Widget
 from kivy.uix.slider import Slider
 from kivy.factory import Factory
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty, NumericProperty, ReferenceListProperty
+from kivy.properties import ObjectProperty, NumericProperty, ReferenceListProperty, AliasProperty
 
 from valuefader import Fader
 from xypad import XyPad
 
 class ExtendedXyPad(XyPad):
-    xfader = ObjectProperty(None)
-    yfader = ObjectProperty(None)
-
-    
+    xfader = ObjectProperty(Fader)
+    yfader = ObjectProperty(Fader)
+                    
+    def update_cursor_from_pad(self, obj, touch):
+        if obj.collide_point(touch.pos[0], touch.pos[1]):
+            self.xfader.value_pos = touch.pos
+            self.yfader.value_pos = touch.pos
 
 class ExtendedXyPadApp(App):
     def build(self):
