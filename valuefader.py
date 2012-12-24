158161e537afdeb8758c1e8479a2cfff7ea4ba67
orl <orl@debian>
2012-12-24 14:03:59 +0100

    ValueFader will now inherit from Fader

--- valuefader.py
+++ valuefader.py
@@ -15,32 +15,38 @@ class Fader(Slider):
     b = NumericProperty()
     color = ReferenceListProperty(r, g, b)
 
+    subpad = NumericProperty()
+
     def get_value_pos(self):
         padding = self.padding
+        subpad = self.subpad
         x = self.x
         y = self.y
         nval = self.value_normalized
         if self.orientation == 'horizontal':
             return (x + padding + nval * (self.width - 2 * padding), y)
         else:
-            return (x, y + padding + nval * (self.height - 2 * padding))
+            return (x, y + subpad + padding + nval * (self.height - 2 * padding - subpad))
 
     def set_value_pos(self, pos):
-        x = min(self.right - self.padding, max(pos[0], self.x + self.padding))
-        y = min(self.top - self.padding, max(pos[1], self.y + self.padding))
+        padding = self.padding
+        subpad = self.subpad
+        x = min(self.right, max(pos[0], self.x + padding))
+        y = min(self.top, max(pos[1], self.y + padding + subpad))
         if self.orientation == 'horizontal':
             if self.width == 0:
                 self.value_normalized = 0
             else:
-                self.value_normalized = (x - self.x - self.padding) / float(self.width - 2 * self.padding)
+                self.value_normalized = (x - self.x - padding) / float(self.width - 2 * padding)
         else:
             if self.height == 0:
                 self.value_normalized = 0
             else:
-                self.value_normalized = (y - self.y - self.padding) / float(self.height - 2 * self.padding)
+                self.value_normalized = (y - self.y - padding - subpad) / float(self.height - 2 * padding - subpad)
     value_pos = AliasProperty(get_value_pos, set_value_pos,
                               bind=('x', 'y', 'width', 'height', 'min',
                                     'max', 'value_normalized', 'orientation'))
+
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
@@ -72,7 +78,7 @@ class ValueFader(Widget):
 
 class ValueFaderApp(App):
     def build(self):
-        return ValueFader(name='My Fader', orientation='horizontal', color=(1,0.5,0.5))
+        return ValueFader(name='My Fader', orientation='vertical', color=(1,0.5,0.5))
 
 Factory.register('Fader', Fader)
 if __name__ == "__main__":
