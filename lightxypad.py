eb0b5b2cadf48d5623bc85aaaea73cc340f20b4f
orl <orl@debian>
2012-12-29 19:42:17 +0100

    Check with intermediate AliasProperty

--- lightxypad.py
+++ lightxypad.py
@@ -13,6 +13,12 @@ from extendedxypad import ExtendedXyPad
 class LightXyPad(ExtendedXyPad):
     hue_w = ObjectProperty(Fader)
 
+    def get_hue(self):
+        return(self.hue_w.value)
+    def set_hue(self, value):
+        self.hue_w.value = value
+    value = AliasProperty(get_hue, set_hue)
+
     def get_hsv_triplet(self):
         return(self.hue_w.value, self.value[0], self.value[1])
 
@@ -21,7 +27,7 @@ class LightXyPad(ExtendedXyPad):
         self.value = (value[1], value[2])
 
     hsv = AliasProperty(get_hsv_triplet, set_hsv_triplet, 
-                        bind=('value', 'hue_w.value'))
+                        bind=('value', 'hue'))
 
 class LightXyPadApp(App):
     def build(self):
