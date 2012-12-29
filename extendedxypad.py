9afa1b69230bb406574df629753c2f09b23ed967
orl <orl@debian>
2012-12-29 16:27:51 +0100

    extendedxypad fixs in progress

--- extendedxypad.py
+++ extendedxypad.py
@@ -5,7 +5,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.factory import Factory
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty, NumericProperty, ReferenceListProperty, AliasProperty
+from kivy.properties import ObjectProperty
 
 from valuefader import Fader
 from xypad import XyPad
@@ -13,6 +13,7 @@ from xypad import XyPad
 class ExtendedXyPad(XyPad):
     xfader = ObjectProperty(Fader)
     yfader = ObjectProperty(Fader)
+
                     
     def update_cursor_from_pad(self, obj, touch):
         if (touch.x < obj.x + obj.width + 5) & (touch.x > obj.x + 5) & (touch.y < obj.y + obj.height + 5) & (touch.y > obj.y - 5):
@@ -25,7 +26,6 @@ class ExtendedXyPadApp(App):
         return ExtendedXyPad(color=(1,0,0), name='My Xtended XY Pad', x_name='X', y_name='Y', step=(0.01, 0.01))
 
 
-#Factory.register('XyPad', XyPad)
 Factory.register('Fader', Fader)
 Builder.load_file('xypad.kv')
 Builder.load_file('valuefader.kv')
