94b2986412cb8030c85e126d6c0252c13b33afd6
orl <orl@debian>
2013-01-01 18:35:28 +0100

    Clean-ups

--- lightxypad.py
+++ lightxypad.py
@@ -18,20 +18,6 @@ class LightXyPad(ExtendedXyPad):
     x_name_pad = 120
     faders_subpad = (80, 0)
 
-    # i_h = NumericProperty()
-    # i_s = NumericProperty()
-    # i_v = NumericProperty()
-    # i_color = ReferenceListProperty(i_h, i_s, i_v)
-
-    # def on_hue_w(self, *args):
-    #     self.hue_w.bind(value=self.setter('hue'))
-
-    # def get_hue(self):
-    #     return(self.hue_w.value)
-    # def set_hue(self, value):
-    #     self.hue_w.value = value
-    # hue = AliasProperty(get_hue, set_hue)
-
     def get_hsv_triplet(self):
         return(self.hue_w.value, self.value[0], self.value[1])
 
@@ -40,11 +26,6 @@ class LightXyPad(ExtendedXyPad):
         self.value = (value[1], value[2])
     hsv = AliasProperty(get_hsv_triplet, set_hsv_triplet, 
                         bind=('value', 'hue'))
-
-#    def change_bg(self):
-#        with self.canvas:
-#            Color(self.hue, self.hsv[1], self.hsv[2], mode='hsv')
-#            Rectangle(pos=(self.x, 20), size=(self.width, 60))
 
 class LightXyPadApp(App):
     def build(self):
