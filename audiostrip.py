d98f63262cf974225f1b18a5c8317674f143a70e
orlammd <orl@ammd.net>
2013-01-10 22:26:50 +0100

    Design boutons OK

--- audiostrip.py
+++ audiostrip.py
@@ -1,25 +1,26 @@
 import kivy
-kivy.require('1.4.1')
+kivy.require('1.5.1')
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from valuefader import ValueFader
 from kivy.uix.togglebutton import ToggleButton
 from kivy.properties import ObjectProperty, StringProperty, NumericProperty, ReferenceListProperty
 from kivy.factory import Factory
 from kivy.lang import Builder
 
+from valuefader import ValueFader
+from toggle import Toggle
 
-
-class Mute(ToggleButton):
-    path = StringProperty('Mute')
+#class Mute(ToggleButton):
+ #   path = StringProperty('Mute')
 
 class AudioStrip(Widget):
     path = StringProperty('Strip')
     name = StringProperty()
     gainfader = ObjectProperty(None)
     panfader = ObjectProperty(None)
-    mute = ObjectProperty(None)
+    mute = Toggle()
+#    mute = ObjectProperty(None)
 #    name_s = ObjectProperty(None)
     r = NumericProperty()
     g = NumericProperty()
@@ -32,8 +33,9 @@ class AudioStripApp(App):
     def build(self):
         return AudioStrip(name="My Strip", color = (1, 0.5, 0.5))
 
+Builder.load_file('toggle.kv')
 Builder.load_file('valuefader.kv')
-Factory.register('ValueFader', ValueFader)
-Factory.register('Mute', Mute)
+#Factory.register('ValueFader', ValueFader)
+#Factory.register('Mute', Mute)
 if __name__ == '__main__':
     AudioStripApp().run()
--- toggle.py
+++ toggle.py
@@ -14,13 +14,23 @@ class Toggle(ToggleButton, OscSender):
     r = NumericProperty()
     g = NumericProperty()
     b = NumericProperty()
-    color = ReferenceListProperty(r, g, b)
+    main_color = ReferenceListProperty(r, g, b)
+
+    def control_cb(self, path, args, types, src):
+        if re.search('127.0.0.1', src.get_url()):
+            print "self-incoming message"
+        else:
+            print "OSC controlled"
+            if float(args[0]):
+                self.state = 'down'
+            else:
+                self.state = 'normal'
 
 class ToggleApp(App):
     name = StringProperty()
     name = "kvGhislame"
     def build(self):
-        return Toggle(name="My Toggle", color=(1,0,0), app_name=self.name)
+        return Toggle(name="My Toggle", color=(1,0,0,1), app_name=self.name)
 
 if __name__ == "__main__":
     ToggleApp().run()
