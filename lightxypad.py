a82e5d00921df064a11f6128105a56bac56555b7
orlammd <orl@ammd.net>
2013-01-13 09:08:03 +0100

    LightXyPad now can be used within other widgets - positionning problems fixed

--- lightxypad.py
+++ lightxypad.py
@@ -46,10 +46,10 @@ class LightXyPad(ExtendedXyPad):
             if self.collide_point(touch.x, touch.y):
                 self.hue_w.value_pos = touch.pos
         else:
-            if touch.x > self.subpad[0] + self.padding:
+            if touch.x > self.x + self.subpad[0] + self.padding:
                 self.xfader.value_pos = touch.pos
                 new_value_x = touch.x
-            if touch.y > self.subpad[1] + self.padding:
+            if touch.y > self.y + self.subpad[1] + self.padding:
                 self.yfader.value_pos = touch.pos
                 new_value_y = touch.y
             self.value_pos = (new_value_x, new_value_y)
