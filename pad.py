f0e40facaa99a0d4e626aa0d8211800818c565b6
orlammd <orl@ammd.net>
2013-01-20 21:13:19 +0100

    fix some positionning problems

--- pad.py
+++ pad.py
@@ -99,8 +99,8 @@ class Pad(Widget):
 
     def get_value_pos(self):
         padding = self.padding
-        x = self.pad_x
-        y = self.pad_y
+        x = self.x + self.pad_x
+        y = self.y + self.pad_y
         nval = self.value_normalized
         xpos = x + padding
         ypos = y + padding
@@ -109,9 +109,9 @@ class Pad(Widget):
     def set_value_pos(self, pos):
         padding = self.padding
         x = min(self.right - padding, max(pos[0], self.pad_x + padding))
-        xpos = self.pad_x + padding
+        xpos = self.x + self.pad_x + padding
         y = min(self.top - padding, max(pos[1], self.pad_y + padding))
-        ypos = self.pad_y + padding
+        ypos = self.y + self.pad_y + padding
         self.value_normalized = ((x - xpos) / float(self.pad_width - 2 * padding) , (y - ypos) / float(self.pad_height - 2 * padding))
     value_pos = AliasProperty(get_value_pos, set_value_pos,
                               bind=('x', 'y', 'width', 'height', 'min',
