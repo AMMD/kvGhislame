4ed70d3cca0aa8388876fa689f653c539d57652c
orlammd <orl@ammd.net>
2013-02-03 23:25:58 +0100

    inifinite loop on multiple light strips

--- multiplelightstrips.py
+++ multiplelightstrips.py
@@ -27,7 +27,7 @@ class MultipleLightStrips(ScreenManager):
 
     lb1 = ObjectProperty(LightBox)
     lb2 = ObjectProperty(LightBox)
-    lb3 = ObjectProperty(LightXyPad)
+    lb3 = ObjectProperty(LightBox)
 
     name = StringProperty()
     name1 = StringProperty()
